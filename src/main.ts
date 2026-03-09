2bb4a6f3b69442236edbf3c64ad3a5a88bc929c9
Lieuwe Jansons <lieuwe.jansons@ns.nl>
2026-03-09 16:38:04 +0100

    refactor-rename-game-selection: Fixed potential lifecycle issue in PR. Renamed GameHistory internals.

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,31 @@
-import { IInputAdapter, TerminalInputAdapter } from './core/adapters/terminalInputAdapter';
-import { IOutputAdapter, TerminalOutputAdapter } from './core/adapters/terminalOutputAdapter';
-import { GameFactory, IGameFactory } from './game-bootstrap/gameFactory';
-import {
-  GameDescriptor,
-  LISTED_GAMES,
-  UNLISTED_GAMES,
-} from './game-bootstrap/composition/games-config';
-import {
-  MainMenuSelectionService,
-  IMenuSelectionService,
-} from './game-bootstrap/mainMenuSelectionService';
+import { TerminalInputAdapter } from './core/adapters/terminalInputAdapter';
+import { TerminalOutputAdapter } from './core/adapters/terminalOutputAdapter';
+import { GameFactory } from './game-bootstrap/gameFactory';
+import { LISTED_GAMES, UNLISTED_GAMES } from './game-bootstrap/composition/games-config';
+import { MainMenuSelectionService } from './game-bootstrap/mainMenuSelectionService';
 import { PlayerSelectionService } from './game-bootstrap/playerSelectionService';
-import { IGame } from './core/game';
 
-type IMainInput = {
-  inputAdapter: IInputAdapter;
-  outputAdapter: IOutputAdapter;
-  mainMenuService: IMenuSelectionService<GameDescriptor | null>;
-  gameFactory: IGameFactory<IGame>;
-};
+async function main() {
+  let outputAdapter: TerminalOutputAdapter | undefined;
+  let inputAdapter: TerminalInputAdapter | undefined;
 
-async function main({ inputAdapter, outputAdapter, mainMenuService, gameFactory }: IMainInput) {
   try {
+    inputAdapter = new TerminalInputAdapter(process.stdin, process.stdout);
+    outputAdapter = new TerminalOutputAdapter();
+
+    const mainMenuService = new MainMenuSelectionService(
+      inputAdapter,
+      outputAdapter,
+      LISTED_GAMES,
+      UNLISTED_GAMES
+    );
+
+    const gameFactory = new GameFactory(
+      inputAdapter,
+      outputAdapter,
+      new PlayerSelectionService(inputAdapter, outputAdapter)
+    );
+
     while (true) {
       const userSelection = await mainMenuService.select();
 
@@ -37,28 +41,16 @@ async function main({ inputAdapter, outputAdapter, mainMenuService, gameFactory 
       outputAdapter.render('\nPractice makes perfect!');
     }
   } catch (error) {
-    outputAdapter.render('An unexpected error occurred.');
+    if (outputAdapter) {
+      outputAdapter.render('An unexpected error occurred.');
+    }
+
     console.error(error);
   } finally {
-    inputAdapter.close();
+    if (inputAdapter) {
+      inputAdapter.close();
+    }
   }
 }
 
-const inputAdapter = new TerminalInputAdapter(process.stdin, process.stdout);
-const outputAdapter = new TerminalOutputAdapter();
-
-main({
-  inputAdapter,
-  outputAdapter,
-  mainMenuService: new MainMenuSelectionService(
-    inputAdapter,
-    outputAdapter,
-    LISTED_GAMES,
-    UNLISTED_GAMES
-  ),
-  gameFactory: new GameFactory(
-    inputAdapter,
-    outputAdapter,
-    new PlayerSelectionService(inputAdapter, outputAdapter)
-  ),
-});
+main();
